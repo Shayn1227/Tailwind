b409d1d06a9af10ba70207045f7f9f5ed268feca
Shayn1227 <antonyjamarques@gmail.com>
2023-05-03 15:29:12 -0300

    Começo do projeto facebook da udemy

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,25 @@ module.exports = {
       spacing: {
         '14': '3.5rem',
         '125px': '125px',
+        '30': '7.5rem',
+        '290px': '290px',
+        '396': '396',
+        '500px': '500px',
+        '980px': '980px'
       },
       colors: {
         'hoverspt': '#18D760',
         'purple-main': '#2D46B9',
-        'green-main': '#1ED760'
+        'green-main': '#1ED760',
+        'main-gray': '#F0F2F5',
+        'main-blue': '#1877F2',
+        'main-blue-hover': '#166FE5',
+        'main-green': '#42B72A',
+        'main-green-hover': '#36AA20'
       },
       backgroundImage: theme => ({
-        'spotify-theme': "url('img/bursts.svg')",
-        'spotify-theme-mobile': "url('img/bursts-mobile.svg')"
+        'spotify-theme': 'url(../img/bursts.svg)',
+        'spotify-theme-mobile': 'url(img/bursts-mobile.svg)'
       }),
       backgroundSize: {
         '175%': '175%',
@@ -26,7 +36,8 @@ module.exports = {
         'banner-mobile': 'top 25% center'
       },
       fontSize: {
-        '9xl': "9rem"
+        '9xl': '9rem',
+        'subtitle': '1.7rem'
       },
       fontFamily: {
         'arial': 'Arial'
